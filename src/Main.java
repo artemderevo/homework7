76824cb3cde75cf843184428873d06643db66eec
q2q2q8 <art-kash@mail.ru>
2022-10-31 13:08:54 +0300

    четвертое задание

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 17; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i);
-
-            }
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
 
         }
 
